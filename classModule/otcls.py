37af2764590c2151c3fadcd366fed5ef03b2d16c
Ming-Ting <cosoet@gmail.com>
2018-09-17 18:57:03 -0400

    Add the class module and HowToUseClass.ipynb

---
+++ classModule/otcls.py
@@ -0,0 +1,24 @@
+class Families:
+    def __init__(self, ID):
+        self.ID = ID
+        self.Maried = "NA"
+        self.Divorced = "NA"
+        self.HusbandID = "NA"
+        self.HusbandName = "NA"
+        self.WifeID = "NA"
+        self.WifeName = "NA"
+        self.Children = []
+
+
+class Individuals: # 
+    def __init__(self, ID):
+        self.ID = ID
+        self.Name = "NA"
+        self.Gender = "NA"
+        self.Birthday = "NA"
+        self.Age = "NA"
+        self.Alive = "NA"
+        self.Death = "NA"
+        self.Child = "NA"
+        self.Spouse = "NA"
+
