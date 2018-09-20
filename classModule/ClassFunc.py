43bfcd8ec4c025b5f84a3cde0adc0be4bb7c629c
dar1enyang <corupted5354.cv99@g2.nctu.edu.tw>
2018-09-20 16:06:34 -0400

    modified classFun

--- classModule/ClassFunc.py
+++ classModule/ClassFunc.py
@@ -39,6 +39,29 @@ def DateBeforeCur(test_Year,test_Month,test_Day):
     count=(test_Date-date.today())/datetime.timedelta(days=1)
     return count<0
 
+#US01
+def DateBeforeCur(test_Year,test_Month,test_Day):
+    if type(test_Year)!=int or type(test_Month)!=int or type(test_Day)!=int:
+        print("Wrong data type for input")
+        return False
+    test_Date=datetime.date(test_Year,test_Month,test_Day)
+    count=(test_Date-date.today())/datetime.timedelta(days=1)
+    return count<0
+
+#US06
+def ValidDivorcestr(indList,fList):
+    for each in fList:
+        if each.Divorced!="NA":
+            for eachind in indList:
+                if each.HusbandID==eachind.ID or each.WifeID=eachind.ID:
+                    if eachind.Death!="NA":
+                        death=(datetime.datetime.strptime(eachind.Death,"%Y-%m-%d")-datetime.datetime.strptime(\
+                                                          each.Divorced,"%Y-%m-%d"))/datetime.timedelta(days=1)
+                        if death<0:
+                            print("No"+eachind.Id+": "+eachind.Name+" is dead before divorced. Please check again.")
+                            return False
+                    
+    return True
 
 
 
