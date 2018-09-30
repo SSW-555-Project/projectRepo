192f6ab26289c415b9ae892eafac5358e9747445
Ming-Ting <cosoet@gmail.com>
2018-09-30 17:08:43 -0400

    add function to check date

--- Project04/Sprint01/classModule/sprint1Func.py
+++ Project04/Sprint01/classModule/sprint1Func.py
@@ -1,36 +1,41 @@
 import datetime
 
+def isDataFormat(date):
+    if isinstance(date, str) == True and len(date.split('-')) == 3:
+        year, month, day = date.split('-')
+        if int(month) > 12:
+            return False
+        
+        if int(day) > 31:
+            return False
+        
+        if day[:].isdigit() and month.isdigit() and year.isdigit(): 
+            return True
+        else:
+            return False
+    else:
+        return False
 
 def isBirthBeforeDeath(birthday, deathday):
     # US03 Birth should occur before death of an individual
+    
 
     if isinstance(birthday, datetime.date) != True:
         #msg = "The type of birthday is not datetime"
-        return False
+        if isDataFormat(birthday) == False :
+            return False
+        birthday = datetime.datetime.strptime(birthday, '%Y-%m-%d')
+
+
     
     if isinstance(deathday, datetime.date) != True:
         #msg = "The type of deathday is not datetime"
-        return False
+        if isDataFormat(deathday) == False :
+            return False
+        deathday = datetime.datetime.strptime(deathday, '%Y-%m-%d')
+     
 
     if(deathday - birthday).total_seconds() > 0 :
         return True
     else:
         return False
-
-def isMarriageBeforeDeath(marriageday, deathday):
-    # US05 Marriage should occur before death of either spouse
-    # Assume temporarily that this function will be call twice. (husband and wife)
-
-    if isinstance(marriageday, datetime.date) != True:
-        #msg = "The type of marriageday is not datetime"
-        return False
-    
-    if isinstance(deathday, datetime.date) != True:
-        #msg = "The type of deathday is not datetime"
-        return False
-
-    if(deathday - marriageday).total_seconds() > 0 :
-        return True
-    else:
-        return False
-
