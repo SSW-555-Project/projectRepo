3076d391c0e24ace9193f4fcc23ab030028c4b41
cshih685 <32198222+leehon3322@users.noreply.github.com>
2018-11-11 03:27:20 -0500

    add us38 us39

---
+++ Project04/module/func/sprint4Func.py
@@ -0,0 +1,39 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+import datetime
+
+#US_38_List upcoming birthdays
+def US_38_upcomingBirthdays(pp):
+        currentYear = str(datetime.date.today().year)
+        nextYear = str(datetime.date.today().year+1)
+        if pp.Birthday[5:7] == '01':  #if the birthday is on January, it's possible that occurs 30 days later from December
+            birthString = pp.Birthday.replace(pp.Birthday[:4], nextYear)
+            birthday = datetime.datetime.strptime(birthString, "%Y-%m-%d").date()
+        else:
+            birthString = pp.Birthday.replace(pp.Birthday[:4], currentYear)
+            birthday = datetime.datetime.strptime(birthString, "%Y-%m-%d").date()
+        today = datetime.datetime.now().date()
+        iterval = birthday - today
+        if datetime.timedelta(days = 0) <= iterval <= datetime.timedelta(days=30):
+            #print(pp.ID,pp.Name, birthday)
+            return True
+        
+#US_39_List upcoming anniversaries
+def US_39_upcomingAnniversaries(fm):
+    currentYear = str(datetime.date.today().year)
+    nextYear = str(datetime.date.today().year+1)
+    if fm.Married[5:7] == '01':
+        marriedString = fm.Married.replace(fm.Married[:4], nextYear)
+        marriedDay = datetime.datetime.strptime(marriedString, "%Y-%m-%d").date()
+    else:
+        marriedString = fm.Married.replace(fm.Married[:4], currentYear)
+        marriedDay = datetime.datetime.strptime(marriedString, "%Y-%m-%d").date()
+    today = datetime.datetime.now().date()
+    iterval = marriedDay - today
+    if datetime.timedelta(days = 0) <= iterval <= datetime.timedelta(days=30):
+        #print(fm.ID, marriedDay)
+        return True
